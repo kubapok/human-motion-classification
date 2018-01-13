688d15d47f73bbfa88308356811b1d882d79554e
Jakub Pokrywka <jakubpokrywka@gmail.com>
2018-01-13 13:32:41 +0100

    draft added

---
+++ draft.py
@@ -0,0 +1,24 @@
+import numpy as np
+import cv2
+import sys
+
+cap = cv2.VideoCapture(sys.argv[1])
+
+fgbg = cv2.bgsegm.createBackgroundSubtractorMOG()
+
+i = 0 
+while(1):
+    ret, frame = cap.read()
+
+    fgmask = fgbg.apply(frame)
+
+    cv2.imshow('frame',fgmask)
+    i+=1
+    if i == 20:
+        from IPython import embed; embed()
+    k = cv2.waitKey(30) & 0xff
+    if k == 27:
+        break
+
+cap.release()
+cv2.destroyAllWindows()

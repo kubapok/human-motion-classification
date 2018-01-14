e01596723aa9a17abc87a31c49b631f19f0ecc24
Jakub Pokrywka <jakubpokrywka@gmail.com>
2018-01-14 18:54:26 +0100

    add optical flow ROI direction (refactoring needed

--- draft.py
+++ draft.py
@@ -13,6 +13,8 @@ ROIframe = np.zeros((480, 640), dtype=np.uint8)
 mask = np.zeros((480, 640), dtype=np.uint8)
 ret, frame = cap.read()
 prvs = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+lindex, rindex, uindex, dindex, = 0, 10, 0, 10
+i = 0
 while(1):
     ret, frame = cap.read()
 
@@ -22,18 +24,27 @@ while(1):
         if lindex and rindex and dindex and uindex:
             mask = np.zeros((480, 640), dtype=np.uint8)
             mask[dindex: uindex, lindex:rindex] = 255
-            ROIframe = cv2.bitwise_and(frame, frame, mask=mask)
             frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-            flow = cv2.calcOpticalFlowFarneback(prvs,frame_gray, None, 0.5, 3, 15, 3, 5, 1.2, 0)
-            from IPython import embed; embed()
-            prvs = frame_gray
-            cv2.rectangle(frame, (lindex, dindex),
-                          (rindex, uindex), (0, 255, 0), 3)
+            ROIframe_gray = cv2.bitwise_and(frame_gray, frame_gray, mask=mask)
+            flow = cv2.calcOpticalFlowFarneback(
+                prvs, ROIframe_gray, None, 0.5, 3, 15, 3, 5, 1.2, 0)
+            ROIonly_flow_gray = flow[dindex: uindex, lindex:rindex]
+            vertical_movement, horizontal_movement = cv2.split(
+                ROIonly_flow_gray)
+            print(vertical_movement.sum(), '\t' * 5, horizontal_movement.sum())
+            prvs = ROIframe_gray
+            i += 1
+            cv2.line(frame, (400, 100), (400+int(vertical_movement.sum()/1000), 100 + int(horizontal_movement.sum() / 1000)), (255, 0, 0), 5)
+            cv2.line(frame, (400, 100), (400,100), (0, 0, 255), 5)
+            if i == 60:
+                pass
+    cv2.rectangle(frame, (lindex, dindex),
+                  (rindex, uindex), (0, 255, 0), 3)
+    # cv2.rectangle(frame, (240, 320),
+    #               (242, 322), (0, 0, 255), 3)
+    # cv2.line(frame, (400, 100), (400+vertical_movement.sum()/1000, 100 + horizontal_movement.sum() / 1000), (255, 0, 0), 5)
 
-    # i += 1
-    # if i == 50:
-    #     from IPython import embed; embed()
-    cv2.imshow('frame', mask)
+    cv2.imshow('frame', frame)
     k = cv2.waitKey(30) & 0xff
     if k == 27:
         break
